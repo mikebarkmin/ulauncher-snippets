c468ec53f31f5a0d39d62580efc053f7489d3cfd
Mike Barkmin <mike.barkmin@uni-due.de>
2021-12-11 23:36:50 +0100

    improve fuzzyfinder

--- src/functions.py
+++ src/functions.py
@@ -206,47 +206,19 @@ def import_file(full_name, path):
     return mod
 
 
-def fuzzyfinder(input, collection, accessor=lambda x: x, sort_results=True):
+def fuzzyfinder(search: str, items: List[str]) -> List[str]:
     """
-    Args:
-        input (str): A partial string which is typically entered by a user.
-        collection (iterable): A collection of strings which will be filtered
-                               based on the `input`.
-        accessor (function): If the `collection` is not an iterable of strings,
-                             then use the accessor to fetch the string that
-                             will be used for fuzzy matching.
-        sort_results(bool): The suggestions are sorted by considering the
-                            smallest contiguous match, followed by where the
-                            match is found in the full string. If two suggestions
-                            have the same rank, they are then sorted
-                            alpha-numerically. This parameter controls the
-                            *last tie-breaker-alpha-numeric sorting*. The sorting
-                            based on match length and position will be intact.
-    Returns:
-        suggestions (generator): A generator object that produces a list of
-            suggestions narrowed down from `collection` using the `input`.
-
-
-    >>> list(fuzzyfinder("al", ["hi", "hu", "hallo", "false"]))
-    ['false', 'hallo']
+    >>> fuzzyfinder("hallo", ["hi", "hu", "hallo", "false"])
+    ['hallo', 'false', 'hi', 'hu']
     """
-    suggestions = []
-    input = str(input) if not isinstance(input, str) else input
-    pat = ".*?".join(map(re.escape, input))
-    # lookahead regex to manage overlapping matches
-    pat = "(?=({0}))".format(pat)
-    regex = re.compile(pat, re.IGNORECASE)
-    for item in collection:
-        r = list(regex.finditer(accessor(item)))
-        if r:
-            # find shortest match
-            best = min(r, key=lambda x: len(x.group(1)))
-            suggestions.append((len(best.group(1)), best.start(), accessor(item), item))
+    scores = []
+    for i in items:
+        score = get_score(search, get_name_from_path(i))
+        scores.append((score, i))
 
-    if sort_results:
-        return (z[-1] for z in sorted(suggestions))
-    else:
-        return (z[-1] for z in sorted(suggestions, key=lambda x: x[:2]))
+    scores = sorted(scores, key=lambda score: score[0], reverse=True)
+
+    return list(map(lambda score: score[1], scores))
 
 
 def random_uuid() -> str:
